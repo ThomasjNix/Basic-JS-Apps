d969d8c6cdafdf921b581e1a54cd3fbc3a90150b
Thomas Nix <tnix2@uncc.edu>
2017-07-02 20:53:42 -0400

    Created a selection sorting micro-app. Updated comments on the bubble-sort.js file to include specification for ES6 and more comments.

--- Sorting/selection-sort.js
+++ Sorting/selection-sort.js
@@ -1,10 +1,5 @@
 /*jshint esversion: 6 */
 
-/*
-For reference, I wholeheartedly understand that bubble sort is awful due to the worst-case performance of O(n^2).
-I am not writing this for the purpose of actually using it, ever, but more so just to show that I know what it is and how it works.
-*/
-
 /*Used elements*/
 var header = document.getElementById('h1_if_needed');
 var textbox = document.getElementById('textbox_if_needed');
@@ -14,18 +9,32 @@ var paragraph = document.getElementById('p_if_needed');
 //Set header instructions
 header.innerHTML = "Enter a list of numbers seperated by single spaces to have them sorted with bubble sort. Please start the entry with a number. Please use at least two numbers (decimals are allowed).";
 
-//Bubble sort function, compares each number to the next in the array, swaps them if need be.
-var bubbleSort = function(list){
-  for (var i = 0; i < list.length - 1; i++){
-    for (var j = 0; j < list.length - i - 1; j++){
-      if (list[j] > list[j+1]){
-        var tempHold = list[j];
-        list[j] = list[j+1];
-        list[j+1] = tempHold;
+/*
+  This is the sorting algorithm, it finds the minimum number and places it at the beginning of the array.
+  Afterwards it moves to the next index of the array and repeats, never changing the index prior.
+  This ensures the next minimum is always at the next index, leaving the maximum at the last index.
+*/
+var selectionSort = function(list, minInVal){
+  for (var i = 0; i < list.length; i++){
+    var minVal = minInVal;
+    for (var j = i; j < list.length; j++){
+      if (list[j] < minVal){
+        minVal = list[j];
       }
+      /*
+      Uncomment this section if you want to see the sort working in action
+      console.log("Iteration: " + i + "\tInner iteration: " + j + "\tCurrent min: " + minVal + "\n");
+      console.log("\t\tArray:" + list);
+      */
     }
+
+    var indexOfMin = list.indexOf(minVal);
+    var tempHold = list[i];
+    list[i] = list[indexOfMin];
+    list[indexOfMin] = tempHold;
   }
 };
+
 
 //Button click event
 button.onclick = function(){
@@ -37,21 +46,27 @@ button.onclick = function(){
   Making sure the list is acceptable, removing excessive white space and
   replacing multiple spaces between entries with a single space,
   then delimiting with spaces to form an array.
+
+  The maximum value is stored for later use in the sorting algorithm.
   */
   var list = textboxVal.replace(/  +/g, ' ').trim().split(' ');
+  var maxVal = 0;
   for (var i = 0; i < list.length; i++){
     if (isNaN(list[i])){
       validText = false;
     }else{
       list[i] = Number(list[i]);
+      if (list[i] > maxVal){
+        maxVal = list[i];
+      }
     }
   }
 
-  //Running the bubble sort if the conditions allow, printing to the paragraph tag once complete.
+  //Running the selection sort if the conditions allow, printing to the paragraph tag once complete.
   //Otherwise alerting that there was an invalid entry and clearing the textbox.
   if (validText){
-    bubbleSort(list);
-    paragraph.innerHTML = "Bubble sorted list: ";
+    selectionSort(list, maxVal);
+    paragraph.innerHTML = "Selection sorted list: ";
     for (var listItem = 0; listItem < list.length; listItem++){
       paragraph.innerHTML += (list[listItem] + ((listItem === list.length - 1) ? "" : ", "));
     }
@@ -60,9 +75,3 @@ button.onclick = function(){
     textbox.value = '';
   }
 };
-
-
-/*
-This is a bubble sort micro-app, one of the worst possible sorting methods to actually ever use.
-But as previously explained, it's just for proof of concept.
-*/
